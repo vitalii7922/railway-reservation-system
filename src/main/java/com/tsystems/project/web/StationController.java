f527909e2bedb80536cf8732ea05b60d0a6800b0
vitalii7922 <nefedovvi792@gmail.com>
2020-03-13 16:07:14 +0300

    commit

--- src/main/java/com/tsystems/project/web/StationController.java
+++ src/main/java/com/tsystems/project/web/StationController.java
@@ -6,17 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/index")
 class StationController {
     @Autowired
     StationService stationService;
 
-    @RequestMapping("/add")
-    public void addStation(@RequestParam("station") String stationName){
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addStation(@RequestParam("station") String stationName){
         Station station = new Station();
         station.setName(stationName);
         stationService.addStation(station);
+        return "Station's been added successfully";
     }
 }
