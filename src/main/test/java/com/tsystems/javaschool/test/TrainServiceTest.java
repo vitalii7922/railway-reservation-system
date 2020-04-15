5a46bd964ed9a4dba6afefb2e3ea3ea11f2c828d
vitalii7922 <nefedovvi792@gmail.com>
2020-04-15 22:47:42 +0300

    commit

--- src/main/test/java/com/tsystems/javaschool/test/TrainServiceTest.java
+++ src/main/test/java/com/tsystems/javaschool/test/TrainServiceTest.java
@@ -14,7 +14,7 @@ import org.junit.Test;
 import org.mockito.*;
 import org.modelmapper.ModelMapper;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 public class TrainServiceTest {
